668b6c2bc4976c18e7a3b10660830a65baa9cf5c
Nikolay Fadin <kuvaldis1988@gmail.com>
2015-01-13 02:08:48 +0300

    spring properties

--- core/src/main/java/kuvaldis/food/core/service/MealServiceImpl.java
+++ core/src/main/java/kuvaldis/food/core/service/MealServiceImpl.java
@@ -3,13 +3,19 @@ package kuvaldis.food.core.service;
 import kuvaldis.food.core.domain.Meal;
 import kuvaldis.food.core.domain.MealCalculationParams;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class MealServiceImpl implements MealService {
+
+    @Value("${some}")
+    private String value;
+
     @Override
     public Meal calculate(MealCalculationParams params) {
+        System.out.println(value);
         log.debug("calculate {}", params);
         return null;
     }
--- core/src/main/java/kuvaldis/food/core/spring/CoreConfig.java
+++ core/src/main/java/kuvaldis/food/core/spring/CoreConfig.java
@@ -2,6 +2,7 @@ package kuvaldis.food.core.spring;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = "kuvaldis.food.core")
