a2449e99fc985748d85aa772d5edd8ba00c29ff9
Nikolay Fadin <kuvaldis1988@gmail.com>
2015-01-15 00:58:42 +0300

    meal calculate result print

--- shell/src/main/java/kuvaldis/food/shell/command/MealCommand.java
+++ shell/src/main/java/kuvaldis/food/shell/command/MealCommand.java
@@ -1,5 +1,12 @@
 package kuvaldis.food.shell.command;
 
+import com.brsanthu.dataexporter.model.AlignType;
+import com.brsanthu.dataexporter.model.Column;
+import com.brsanthu.dataexporter.model.LineNumberColumn;
+import com.brsanthu.dataexporter.model.StringColumn;
+import com.brsanthu.dataexporter.output.text.TextExporter;
+import com.brsanthu.dataexporter.output.texttable.TextTableExporter;
+import kuvaldis.food.core.domain.Course;
 import kuvaldis.food.core.domain.Meal;
 import kuvaldis.food.core.domain.MealCalculationParams;
 import kuvaldis.food.core.domain.MealType;
@@ -8,7 +15,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.core.CommandMarker;
 import org.springframework.shell.core.annotation.CliCommand;
+import org.springframework.shell.core.annotation.CliOption;
 import org.springframework.stereotype.Component;
+
+import java.awt.event.TextEvent;
+import java.io.StringWriter;
 
 @Slf4j
 @Component
@@ -17,9 +28,25 @@ public class MealCommand extends AbstractShellCommandMarker {
     @Autowired
     private MealService mealService;
 
-    @CliCommand(value = "meal calculate")
-    public void mealCalculate() {
-        final Meal meal = mealService.calculate(new MealCalculationParams(MealType.BREAKFAST));
-        System.out.println(meal);
+    @CliCommand(value = "calculate meal", help = "calculate meal --type BREAKFAST")
+    public void calculateMeal(@CliOption(key = "type", mandatory = true) final MealType mealType) {
+        final Meal meal = mealService.calculate(new MealCalculationParams(mealType));
+        handsomePrint(meal);
+    }
+
+    private void handsomePrint(Meal meal) {
+        final StringWriter sw = new StringWriter();
+        sw.append(String.format("%n"));
+        final TextExporter headerExporter = new TextExporter(sw);
+        headerExporter.addColumns(new StringColumn("Courses for meal type: "), new StringColumn(meal.getMealType().toString()));
+        headerExporter.startExporting();
+        headerExporter.finishExporting();
+        sw.append(String.format("%n"));
+        final TextTableExporter coursesExporter = new TextTableExporter(sw);
+        coursesExporter.addColumns(new LineNumberColumn("Line No", 5), new StringColumn("Course name", 20, AlignType.MIDDLE_CENTER));
+        coursesExporter.startExporting();
+        meal.getCourses().stream().forEach(course -> coursesExporter.addRow(course.getName()));
+        coursesExporter.finishExporting();
+        log.info(sw.toString());
     }
 }
